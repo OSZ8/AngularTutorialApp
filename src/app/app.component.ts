ebb4f1df2f573e1c22d5d36c3031f130ceb1d81f
OSZ8 <94046906+OSZ8@users.noreply.github.com>
2024-12-28 17:19:38 +0100

    Actualización del tutorial con ampliaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularTutorialApp';
+  nombre: string = 'Oscar'; // Define la propiedad usada en el HTML
+  mensaje: string = ''; // Propiedad para manejar eventos
+
+  mostrarMensaje() {
+    this.mensaje = '¡Has hecho clic!';
+  }
 }
